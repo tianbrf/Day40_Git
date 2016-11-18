e8e443ce2311118c682512ce7ef2a7688d26e1d1
tqq891226 <895099605@qq.com>
2016-11-18 09:52:52 +0800

    hhh

--- app/src/main/java/com/bq/day40_git/MainActivity.java
+++ app/src/main/java/com/bq/day40_git/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("TQQ", "onCreate: "+"第一次提交" );
+        Log.e("TQQ++++++++", "onCreate: "+"第二次提交" );
     }
 }
